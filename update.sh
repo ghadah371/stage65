046940f4b118e05d9897c9c7223752835da654e0
ghadah371 <ghadahali.ga@gmail.com>
2025-04-15 14:11:09 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+date
+echo "Updating Python application on VM..."
+
+APP_DIR="/home/azureuser/stage6.5"
+REPO_URL="https://github.com/ghadah371/stage65.git"
+BRANCH="main"
+GITHUB_TOKEN=$GITHUB_TOKEN
+
+# Update code
+if [ -d "$APP_DIR" ]; then
+    sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
+else
+    sudo -u azureuser git clone -b $BRANCH "https://${GITHUB_TOKEN}@${REPO_URL}"
+    sudo -u azureuser bash -c "cd $APP_DIR"
+fi
+
+# Install dependencies
+sudo -u azureuser /home/azureuser/stage65/venv/bin/pip install --upgrade pip
+sudo -u azureuser /home/azureuser/stage65/venv/bin/pip install -r ${APP_DIR}/requirements.txt
+
+
+# Restart the service
+sudo systemctl restart backend
+sudo systemctl restart frontend
+
+echo "Python application update completed!"
